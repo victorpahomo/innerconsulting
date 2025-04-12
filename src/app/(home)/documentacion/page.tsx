acf42b227def3aef516aaec1f45cc44bde633a76
victorpahomo <vmmorales@unicauca.edu.co>
2025-04-12 01:20:52 -0500

    feat: add DocumentationPage component with metadata

---
+++ src/app/(home)/documentacion/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Documentación | InnerKanban",
+  description: "Documentación de InnerKanban",
+  keywords: "documentación, innerkanban",
+};
+
+export default function DocumentationPage() {
+  return <div>Documentation</div>;
+}
