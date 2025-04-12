2443c5927fe16e2c69be6a293aa09bee16d3ec74
victorpahomo <vmmorales@unicauca.edu.co>
2025-04-12 01:20:43 -0500

    feat: add AuthorPage component with metadata

---
+++ src/app/(home)/autor/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Victorpahomo | InnerKanban",
+  description: "Autor de InnerKanban",
+  keywords: "autor, innerkanban",
+};
+
+export default function AuthorPage() {
+  return <div>Author</div>;
+}
